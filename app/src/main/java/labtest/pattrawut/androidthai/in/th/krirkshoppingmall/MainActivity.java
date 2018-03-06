23925ee9a00508abc2c9b8f20a24a3f613b2e4a9
aongfootlong <Aong9791>
2018-03-06 11:44:19 +0700

    First Half

---
+++ app/src/main/java/labtest/pattrawut/androidthai/in/th/krirkshoppingmall/MainActivity.java
@@ -0,0 +1,24 @@
+package labtest.pattrawut.androidthai.in.th.krirkshoppingmall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import labtest.pattrawut.androidthai.in.th.krirkshoppingmall.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+        }
+
+
+    }   // Main Method
+
+}   // Main CLass
---
+++ app/src/main/java/labtest/pattrawut/androidthai/in/th/krirkshoppingmall/fragment/MainFragment.java
@@ -0,0 +1,25 @@
+package labtest.pattrawut.androidthai.in.th.krirkshoppingmall.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import labtest.pattrawut.androidthai.in.th.krirkshoppingmall.R;
+
+/**
+ * Created by Pattrawut on 3/6/2018.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+
+    }
+}
